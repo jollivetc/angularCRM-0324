18f9024a7c504adfe6e797eaa481b19d83f43f8a
Christophe Jollivet <jollivet@apside.fr>
2024-02-20 15:06:23 +0100

    validators and tests on components

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 
 @Component({
   selector: 'crm-login',
@@ -13,12 +13,20 @@ export class LoginComponent {
   constructor(){
     this.loginForm = new FormGroup({
       login: new FormControl('', [Validators.required, Validators.minLength(3)]),
-      password: new FormControl('', [Validators.required])
+      password: new FormControl('', [Validators.required, no$InPassword])
     })
   }
 
   onSubmit():void{
     console.log(this.loginForm)
+  }
+}
+
+function no$InPassword(c: AbstractControl):ValidationErrors | null{
+  if((c.value as string).indexOf('$')<0){
+    return null;
+  }else{
+    return {no$InPassword: true};
   }
 
 }
