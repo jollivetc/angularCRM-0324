44f9d4c8ca6e68e8f0bdb3af6987b352cb48cd44
Christophe Jollivet <jollivet@apside.fr>
2024-02-20 11:05:12 +0100

    showing binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularCRM';
+  fruits = [
+    {id:1, variety:'Banane'},
+    {id:2, variety:'Pomme'},
+    {id:3, variety:'Fraise'},
+    {id:4, variety:'Orange'},
+    {id:5, variety:'Myrtille'},
+  ]
+  memory:string='';
+
+
+  isEntering(i:number):void{
+    this.memory = this.fruits[i].variety;
+    this.fruits[i].variety="Cerise"
+  }
+  isLeaving(i:number):void{
+    this.fruits[i].variety=this.memory;
+  }
 }
